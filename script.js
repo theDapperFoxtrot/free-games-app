148f225bf3a29910dce12f337083fd8cc5e3b000
thedapperfoxtrot <thedapperfoxtrot.bizinq@gmail.com>
2024-04-23 22:28:05 +0300

    CORS fix

--- script.js
+++ script.js
@@ -1,62 +1,62 @@
 //url: `https://shrouded-bayou-34065.herokuapp.com/https://www.gamerpower.com/api/giveaways?&type=game&sort-by=popularity`
 
-const app = {}
+const app = {};
 
-app.userInput = function() {
-    const platformChoice = document.querySelector('#platform')
-    document.querySelector('#platform').addEventListener('change', function() {
-        console.log(platformChoice.selectedOptions[0].value)
-        app.queryResolver(platformChoice.selectedOptions[0].value)
-    })
-}
+app.userInput = function () {
+    const platformChoice = document.querySelector('#platform');
+    document.querySelector('#platform').addEventListener('change', function () {
+        console.log(platformChoice.selectedOptions[0].value);
+        app.queryResolver(platformChoice.selectedOptions[0].value);
+    });
+};
 
-app.queryResolver = function(userChoice) {
+app.queryResolver = function (userChoice) {
     if (userChoice === 'epic') {
-        app.apiQuery = 'https://shrouded-bayou-34065.herokuapp.com/https://www.gamerpower.com/api/giveaways?platform=epic-games-store&type=game&sort-by=value'
-        app.fetchData(app.apiQuery)
+        app.apiQuery = 'https://api.allorigins.win/raw?url=https://www.gamerpower.com/api/giveaways?platform=epic-games-store&type=game&sort-by=value';
+        app.fetchData(app.apiQuery);
     } else if (userChoice === 'steam') {
-        app.apiQuery = 'https://shrouded-bayou-34065.herokuapp.com/https://www.gamerpower.com/api/giveaways?platform=steam&type=game&sort-by=value'
-        app.fetchData(app.apiQuery)
+        app.apiQuery = 'https://api.allorigins.win/raw?url=https://www.gamerpower.com/api/giveaways?platform=steam&type=game&sort-by=value';
+        app.fetchData(app.apiQuery);
     } else if (userChoice === 'ubisoft') {
-        app.apiQuery = 'https://shrouded-bayou-34065.herokuapp.com/https://www.gamerpower.com/api/giveaways?platform=ubisoft&type=game&sort-by=value'
-        app.fetchData(app.apiQuery)
+        app.apiQuery = 'https://api.allorigins.win/raw?url=https://www.gamerpower.com/api/giveaways?platform=ubisoft&type=game&sort-by=value';
+        app.fetchData(app.apiQuery);
     } else if (userChoice === 'xbox') {
-        app.apiQuery = 'https://shrouded-bayou-34065.herokuapp.com/https://www.gamerpower.com/api/giveaways?platform=xbox-one&platform=xbox-series-xs&type=game&sort-by=value'
-        app.fetchData(app.apiQuery)
+        app.apiQuery = 'https://api.allorigins.win/raw?url=https://www.gamerpower.com/api/giveaways?platform=xbox-one&platform=xbox-series-xs&type=game&sort-by=value';
+        app.fetchData(app.apiQuery);
     } else if (userChoice === 'gog') {
-        app.apiQuery = 'https://shrouded-bayou-34065.herokuapp.com/https://www.gamerpower.com/api/giveaways?platform=gog&type=game&sort-by=value'
-        app.fetchData(app.apiQuery)
+        app.apiQuery = 'https://api.allorigins.win/raw?url=https://www.gamerpower.com/api/giveaways?platform=gog&type=game&sort-by=value';
+        app.fetchData(app.apiQuery);
     } else if (userChoice === 'all') {
-        app.apiQuery = 'https://shrouded-bayou-34065.herokuapp.com/https://www.gamerpower.com/api/giveaways?type=game&sort-by=value'
-        app.fetchData(app.apiQuery)
+        app.apiQuery = 'https://api.allorigins.win/raw?url=https://www.gamerpower.com/api/giveaways?type=game&sort-by=value';
+        app.fetchData(app.apiQuery);
     }
-}
+};
 
-app.apiQuery = 'https://shrouded-bayou-34065.herokuapp.com/https://www.gamerpower.com/api/giveaways?type=game&sort-by=value'
+app.apiQuery = 'https://api.allorigins.win/raw?url=https://www.gamerpower.com/api/giveaways?type=game&sort-by=value';
 
-app.mainSpace = document.querySelector('#mainContent')
+app.mainSpace = document.querySelector('#mainContent');
 
-app.fetchData = function(input) {
-    app.url = new URL(input)
+app.fetchData = function (input) {
+    app.url = new URL(input);
     fetch(app.url)
-    .then(function(response) {
-        if (response.ok) {
-            return response.json()
-        } else {
-            throw new Error('Something is broken. Check your code.')
-        }
-    })
-    .then(function(jsonResponse) {
-        app.showGames(jsonResponse)
-    })
-}
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Something is broken. Check your code.');
+            }
+        })
+        .then(function (jsonResponse) {
+            app.showGames(jsonResponse);
+        });
+};
 
-app.showGames = function(data) {
-    document.querySelector("#mainContent").innerHTML = ""
-    data.forEach(function(game) {
-        let thisGamehtml = document.getElementById('mainContent')
+app.showGames = function (data) {
+    document.querySelector("#mainContent").innerHTML = "";
+    data.forEach(function (game) {
+        let thisGamehtml = document.getElementById('mainContent');
         thisGamehtml.innerHTML +=
-        `
+            `
         <hr>
         <div class='game'>
         <a href=${game.open_giveaway_url}><h2>${game.title}</h2></a>
@@ -71,12 +71,12 @@ app.showGames = function(data) {
         <a href=${game.open_giveaway_url}>LINK</a>
         <p>How to claim: ${game.instructions}</p>
         </div>
-        `
-    })
-}
+        `;
+    });
+};
 
-app.init = function() {
-    app.userInput()
-}
+app.init = function () {
+    app.userInput();
+};
 
-app.init()
+app.init();
